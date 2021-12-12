257df777b3df38b2a27da205624b7b2c71c3a7bf
Damo <damo@codefishstudio.com>
2021-12-13 09:42:58 +1030

    Move wakelock release

--- android/src/main/java/com/rnandroidblewatcher/RnAndroidBleWatcherJobService.java
+++ android/src/main/java/com/rnandroidblewatcher/RnAndroidBleWatcherJobService.java
@@ -264,7 +264,7 @@ public class RnAndroidBleWatcherJobService extends JobService
       PowerManager.WakeLock wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "RnAndroidBleWatcherJobService:wakelock");
 
 			if (wakeLock.isHeld() == false) {
-							wakeLock.acquire();
+				  wakeLock.acquire();
       }
   }
 
@@ -278,18 +278,19 @@ public class RnAndroidBleWatcherJobService extends JobService
       }
     }
 
+    PowerManager pm = (PowerManager)  context.getSystemService(Context.POWER_SERVICE);
+    PowerManager.WakeLock wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "RnAndroidBleWatcherJobService:wakelock");
+
+		if (wakeLock.isHeld()) {
+				wakeLock.release();
+		}
+
 
     JobScheduler jobScheduler = null;
     jobScheduler = context.getSystemService(JobScheduler.class);
 
     jobScheduler.cancel(JOB_ID);
 
-    PowerManager pm = (PowerManager)  context.getSystemService(Context.POWER_SERVICE);
-    PowerManager.WakeLock wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "RnAndroidBleWatcherJobService:wakelock");
-
-		if (wakeLock.isHeld()) {
-						wakeLock.release();
-		}
 
 
   }
